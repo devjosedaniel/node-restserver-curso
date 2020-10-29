122dd60078adbfa4bb04592923aa2e8acd729df4
Jose Daniel <jose_daniel@outlook.com>
2020-10-29 12:27:12 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/helpDesk'
 } else {
-    urlDB = 'mongodb+srv://administradordb:xntKEsueA9s2WUlq@cluster0.bwfvw.mongodb.net/helpDesk';
+    urlDB = process.env.MONGO_URI;
 }
 
 
